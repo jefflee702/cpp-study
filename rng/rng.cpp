c2fc784ef219c49c8bba6c59d7ee0a1f71de4c5e
jefflee702 <jefflee7020@gmail.com>
2021-08-15 01:39:35 -0700

    For loop added

--- rng/rng.cpp
+++ rng/rng.cpp
@@ -4,10 +4,17 @@ using namespace std;
 int main(){
     srand(time(0));
     int count = 0;
-    while(count < 10){
-        int max = 10;
-        int x = (rand()%max)+1;
-        cout << x << endl;
+    const int MAX = 10;
+    int x = 0;
+    while(count < 10 && x <= 7){
+        x = (rand() % MAX) + 1;
+        cout << count << ") " << x << endl;
         count++;
+    }
+
+// For loop study
+    for(int i=0; i<MAX; i++) {
+        x = (rand() % MAX) + 1;
+        cout << i << ") " << x << endl;
     }
 }
